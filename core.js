3ac9bafe6b95830025d3cd284e99791e6b8e55fe
Nuove Tecnologie Dell'Arte <59918869+nuovetecnologiearte@users.noreply.github.com>
2020-05-15 12:07:25 +0200

    Add files via upload

---
+++ core.js
@@ -0,0 +1,46 @@
+ var count = 0;
+ var arrayImmagini = [];
+ function prelevaImg(){
+    count = 0;
+    // var param ='photooftheday'; //document.getElementById('your_name').value;
+    var url = 'http://api.reddit.com/new';
+    //  document.getElementById('hideAfterClick').style.display='none';
+      var request = new XMLHttpRequest();
+      request.open('GET', url, true);
+
+      request.onload = function() {
+        if (request.status >= 200 && request.status < 400) {
+        // Success!
+          var data = JSON.parse(request.responseText);
+          console.log(data);
+          arrayImmagini = data.data.children;
+          setTimeout(updateDiv, 1000);
+         
+
+        } else {
+          // We reached our target server, but it returned an error
+          console.log('errore nel prelevare il json');
+          setTimeout(prelevaImg, 5000);
+        }
+      };
+
+      request.onerror = function() {
+        // There was a connection error of some sort
+        console.log('il server non risponde');
+      };
+        request.send();
+  }
+
+  function updateDiv(){
+    if (count < arrayImmagini.length ){
+      document.getElementById('image' + Math.floor(Math.random()*9)).innerHTML='<img src="'+arrayImmagini[count].data.thumbnail+'">';
+      count++;
+      setTimeout(updateDiv, 1000);
+    }
+    else {
+      count=0;
+      setTimeout(prelevaImg, 1000);
+    }
+      
+     
+  } 
